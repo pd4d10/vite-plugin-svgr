8c5f27affcdd16823629c8121247f6f8274cfdda
Rongjian Zhang <pd4d10@gmail.com>
2021-01-09 15:53:00 +0800

    feat: initial version

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # vite-plugin-svgr
-Vite plugin to transform SVGs into React components
+
+Vite plugin to transform SVGs into React components. Uses [svgr](https://github.com/gregberge/svgr) under the hood.
+
+## Usage
+
+```js
+// vite.config.js
+import svgr from 'vite-plugin-svgr'
+
+export default {
+  // ...
+  plugins: [svgr()],
+}
+```
+
+## License
+
+MIT
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,38 @@
-console.log('hello')
+import fs from 'fs'
+import type { Plugin } from 'vite'
+import type * as E from 'esbuild'
+
+export = function svgrPlugin(): Plugin {
+  // TODO: options
+  return {
+    name: 'vite:svgr',
+    async transform(code, id) {
+      if (id.endsWith('.svg')) {
+        const svgr = require('@svgr/core').default
+        const esbuild = require('esbuild') as typeof E
+
+        const svg = await fs.promises.readFile(id, 'utf8')
+
+        const componentCode = await svgr(
+          svg,
+          {},
+          { componentName: 'ReactComponent' }
+        ).then((res: string) => {
+          return res.replace(
+            'export default ReactComponent',
+            `export { ReactComponent }`
+          )
+        })
+
+        const res = await esbuild.transform(componentCode + '\n' + code, {
+          loader: 'jsx',
+        })
+
+        return {
+          code: res.code,
+          map: res.map,
+        }
+      }
+    },
+  }
+}
