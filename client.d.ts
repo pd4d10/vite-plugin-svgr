54dbe455a55f79cc739aba89f555ab6b4373ea29
Gabriel Pereira Woitechen <wtchnm@icloud.com>
2021-08-11 09:40:23 -0300

    fix(client.d.ts): src is already exported by Vite (#12)

--- client.d.ts
+++ client.d.ts
@@ -6,7 +6,4 @@ declare module '*.svg' {
   export const ReactComponent: React.FunctionComponent<
     React.SVGProps<SVGSVGElement> & { title?: string }
   >
-
-  const src: string
-  export default src
 }
