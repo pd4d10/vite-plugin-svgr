6ae1c9c1a00997d76cbbf4893a4a07ce621a2021
casperck64@gmail.com <casperck64@gmail.com>
2022-03-22 20:23:02 +0100

    Added option for local styling

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,13 @@ import type { Plugin } from 'vite'
 type Options = {
   svgrOptions?: Config
   esbuildOptions?: Parameters<typeof transformWithEsbuild>[2]
+  localStyle?: boolean
 }
 
 export = function svgrPlugin({
   svgrOptions,
   esbuildOptions,
+  localStyle
 }: Options = {}): Plugin {
   return {
     name: 'vite:svgr',
@@ -18,8 +20,26 @@ export = function svgrPlugin({
       if (id.endsWith('.svg')) {
         const { transform: convert } = await import('@svgr/core')
 
-        const svgCode = await fs.promises.readFile(id, 'utf8')
+        let svgCode = await fs.promises.readFile(id, 'utf8')
 
+        if (localStyle) {
+          const regex = new RegExp(/[>|\}]\.([^\{]+)\{/gm)
+          let match: RegExpExecArray | null; 
+          let classMap : Record<string, string>= {};
+          while ((match = regex.exec(svgCode)) !== null) {
+            let classes = match[1].split(".")
+            classes = classes.map(c => c.replace(",",""))
+            classes.forEach(c => {
+              let uid = Math.random().toString(16).substr(2);
+              classMap[c] = uid;
+            })
+          }
+          let keys = Object.keys(classMap);
+          keys.forEach(key => {
+            svgCode = svgCode.replace(new RegExp(key, "g"), `__svg__${classMap[key]}`)
+          })
+        }
+        
         const componentCode = await convert(svgCode, svgrOptions, {
           componentName: 'ReactComponent',
           filePath: id,
