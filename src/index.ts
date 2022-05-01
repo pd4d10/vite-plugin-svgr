7e6a069f7c4b7b0aa99c46d7329adcf1c92213e2
Rongjian Zhang <pd4d10@gmail.com>
2022-05-01 11:07:47 +0800

    Update src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ type Options = {
   esbuildOptions?: Parameters<typeof transformWithEsbuild>[2]
 }
 
-export deafult function svgrPlugin({
+export default function svgrPlugin({
   svgrOptions,
   esbuildOptions,
 }: Options = {}): Plugin {
