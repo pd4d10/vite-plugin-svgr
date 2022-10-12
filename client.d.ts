0f108e1feccb499c6f23a82742fcaa186b47bc87
Valery Smirnov <57757211+XantreGodlike@users.noreply.github.com>
2022-10-12 22:53:22 +0600

    fixed type issues with preact

--- client.d.ts
+++ client.d.ts
@@ -4,6 +4,6 @@ declare module '*.svg' {
   import * as React from 'react'
 
   export const ReactComponent: React.FunctionComponent<
-    React.SVGProps<SVGSVGElement> & { title?: string }
+    React.ComponentProps<'svg'> & { title?: string }
   >
 }
