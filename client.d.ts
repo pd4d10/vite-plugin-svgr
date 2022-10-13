6f5726cff8529d3b469b5dbd53b1a6e345bb81a4
Rongjian Zhang <pd4d10@gmail.com>
2022-10-13 16:09:22 +0800

    Update client.d.ts

--- client.d.ts
+++ client.d.ts
@@ -1,4 +1,5 @@
 // https://github.com/facebook/create-react-app/blob/0ee4765c39f820e5f4820abf4bf2e47b3324da7f/packages/react-scripts/lib/react-app.d.ts#L47-L56
+// https://github.com/pd4d10/vite-plugin-svgr/pull/56 for preact compatiblility
 
 declare module '*.svg' {
   import * as React from 'react'
