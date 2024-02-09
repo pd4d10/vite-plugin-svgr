242e6a605ffe65094cc025bb4b97291befc89ca3
Himanshu Patil <himanshupatil744@gmail.com>
2024-02-09 22:35:23 +0530

    chore: added codemods to migrate v3 -> v4 (#104)

---
+++ codemods/src/v4/default-export/default-export.js
@@ -0,0 +1,32 @@
+module.exports = function (file, api) {
+    const jscodeshift = api.jscodeshift;
+    const root = jscodeshift(file.source);
+
+    root.find(jscodeshift.ImportDeclaration).forEach((path) => {
+        const importPath = path.node.source.value;
+
+        if (importPath.endsWith('.svg')) {
+
+            const hasDefaultSpecifier = path.node.specifiers.some(specifier =>
+                jscodeshift.ImportDefaultSpecifier.check(specifier)
+            );
+
+            // Skip transformation if there is a default import specifier
+            if (!hasDefaultSpecifier) {
+                const updatedImportPath = `${importPath}?react`;
+
+                path.node.specifiers.forEach(specifier => {
+                    if (jscodeshift.ImportSpecifier.check(specifier)) {
+                        // Convert named import to default import
+                        const newSpecifier = jscodeshift.importDefaultSpecifier(jscodeshift.identifier(specifier.local.name));
+                        specifier.type = 'ImportDefaultSpecifier';
+                        specifier.local = newSpecifier.local;
+                    }
+                });
+
+                path.node.source = jscodeshift.literal(updatedImportPath);
+            }
+        }
+    });
+    return root.toSource();
+};
